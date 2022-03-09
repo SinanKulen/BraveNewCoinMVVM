6ab19521ee3b83e3b33efa20aad1b7f8b094e8f1
Sinan Kulen <sinan.kulen@gmail.com>
2022-03-09 14:19:33 +0300

    fixed bugs

--- BraveNewCoinMVVM/Model/Responses/AssetDetailResponses.swift
+++ BraveNewCoinMVVM/Model/Responses/AssetDetailResponses.swift
@@ -7,36 +7,9 @@
 
 import Foundation
 
-struct AssetDetailResponses : Decodable
-{
-    private enum RootCodingKeys : String, CodingKey
-    {
-        case id,name,symbol,slugName,status,type,url,contractAddress
-    }
-    
-    var assetDetail = AssetDetail(id: "", name: "", symbol: "", slugName: "", status: "", type: "", url: "", contractAddress: "")
+struct AssetDetailResponses : Decodable {
     var id, name, symbol, slugName, status, type: String
     var url, contractAddress: String?
-    
-    init(from decoder: Decoder) throws {
-        let rootContainer = try decoder.container(keyedBy: RootCodingKeys.self)
-        id = try rootContainer.decode(String.self, forKey: .id)
-        assetDetail.id = id
-        name = try rootContainer.decode(String.self, forKey: .name)
-        assetDetail.name = name
-        symbol = try rootContainer.decode(String.self, forKey: .symbol)
-        assetDetail.symbol = symbol
-        slugName = try rootContainer.decode(String.self, forKey: .slugName)
-        assetDetail.slugName = slugName
-        status = try rootContainer.decode(String.self, forKey: .status)
-        assetDetail.status = status
-        type = try rootContainer.decode(String.self, forKey: .type)
-        assetDetail.type = type
-        url = try rootContainer.decode(String.self, forKey: .url)
-        assetDetail.url = (url != nil) ? url : ""
-        contractAddress = try rootContainer.decode(String.self, forKey: .contractAddress)
-        assetDetail.contractAddress = (contractAddress != nil) ? contractAddress : ""
-    }
 }
 
 
